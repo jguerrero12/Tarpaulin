c826525b9e4ace3676708a88337668a366f9c870
Jose A. Guerrero <jose3292@gmail.com>
2017-03-14 19:47:38 -0400

    Initial Commit

---
+++ Tarpaulin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarpaulin
+//
+//  Created by Jose Guerrero on 3/14/17.
+//  Copyright © 2017 Jose Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
